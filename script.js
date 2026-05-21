70051e9d8e783a5106d1c183b759d72330162b7e
Ajimi Mahdi <ajimimahdi1@gmail.com>
2026-05-21 14:14:23 +0200

    snack 2 ho creato un arrow fuction quadrato ho messo il paramentro numero e lo ritornato numero per se stesso  snack 3 da definire ancora non lo capito bene snack 4 ho creato una funzione creatimer con paramentro ms millisecondo ho ritornato una funzione vuota che al suo interno ce il setTimeout con la funzione che restituisce un cosole con il messaggio in ms e creato una variabile avvia = creaTimer 3000 3 secondi ogni tre secondi mi restituisce tempo scaduto dopo avvio con il messaggio timer avviato 3 secondi restituisce tempo scaduto snack 5 ho creato una funzione stampaognisecondo con il paramentro messaggio dove setInterval ha una funzione che stampa il messaggio in 1 secondo che stampaognisecondo scrive una messaggio scrivimi ogni secondo

--- script.js
+++ script.js
@@ -25,3 +25,61 @@ console.log(somma(5,5));
 console.log(sommaAnonima(4,7));
 console.log(arrowFunction(10,3));
 
+
+/*🏆 Snack 2
+Crea una arrow function che calcola il quadrato di un numero.
+Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.*/
+
+const quadrato = (numero) => {
+    return numero * numero ;
+}
+
+console.log(quadrato(5));
+
+/*🏆 Snack 3
+Crea una funzione eseguiOperazione
+Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). 
+La funzione deve eseguire l'operazione fornita sui due numeri.
+*/
+
+function eseguiOperazione (a, b){
+
+
+};
+
+
+/*Crea un generatore di funzioni creaTimer
+Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione 
+che avvia un setTimeout per stampare "Tempo scaduto!".*/
+
+function creaTimer (ms){
+    return function (){ 
+        setTimeout(function(){
+            console.log("tempo scaduto!")
+        },ms);
+        
+    };
+};
+const avviaCreaTimer= creaTimer(3000);
+
+console.log("timer avviato a " , avviaCreaTimer());
+ 
+/*🏆 Snack 5
+Crea una funzione stampaOgniSecondo con setInterval.
+Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
+
+Nota: Questa funzione creerà un loop infinito. Interrompilo manualmente o usa clearInterval() in un altro script.*/
+
+ 
+function stampaOgniSecondo(messaggio){
+    setInterval(function(){
+            console.log(messaggio)
+        },1000)
+    };
+     stampaOgniSecondo("scrivimi ogni secondo");
+
+
+     /*🏆 Snack 6
+Crea un contatore automatico con setInterval
+Definisci una funzione creaContatoreAutomatico che accetta un intervallo di tempo e restituisce una funzione che avvia un setInterval, 
+incrementando un contatore e stampandolo.*/
